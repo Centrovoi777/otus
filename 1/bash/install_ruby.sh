06c11567152545c066ba5fc8b878306e0c4c9c03
Centrovoi777 <centrovoi777@yandex.ru>
2020-07-13 16:06:57 +0300

    add ubuntu16.json copy script

--- 1/bash/install_ruby.sh
+++ 1/bash/install_ruby.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # install ruby
 echo 'update repository'
-apt update
+sudo apt update
 echo 'install ruby'
-apt install -y ruby-full ruby-bundler build-essential
+sudo apt install -y ruby-full ruby-bundler build-essential
 echo 'check installation'
 ruby -v
 bundler -v
