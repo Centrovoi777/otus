60c65e5405f8fc3ba67f907e50d49244d9afdefc
Centrovoi777 <centrovoi777@yandex.ru>
2020-07-15 09:06:29 +0300

    add mc

--- packer-base/script/install_ruby.sh
+++ packer-base/script/install_ruby.sh
@@ -3,7 +3,7 @@
 echo 'update repository'
 apt update
 echo 'install ruby'
-apt install -y ruby-full ruby-bundler build-essential
+apt install -y ruby-full ruby-bundler build-essential mc
 echo 'check installation'
 ruby -v
 bundler -v
